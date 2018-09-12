5eb13f8d6266f401b392b7388112a4368b964779
Milos Jovancevic <milosjovancevic@Miloss-MacBook-Pro.local>
2018-09-12 15:26:40 +0200

    end of section 5, finished all of the CRUD methods

--- src/features/event/EventList/EventList.jsx
+++ src/features/event/EventList/EventList.jsx
@@ -3,11 +3,16 @@ import EventListItem from "./EventListItem";
 
 class EventList extends Component {
   render() {
-    const { events } = this.props;
+    const { events, onEventOpen, deleteEvent } = this.props;
     return (
       <div>
-        {events.map((event) => (
-          <EventListItem key={event.id} event={event} />
+        {events.map(event => (
+          <EventListItem
+            deleteEvent={deleteEvent}
+            onEventOpen={onEventOpen}
+            key={event.id}
+            event={event}
+          />
         ))}
       </div>
     );
