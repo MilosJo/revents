d6079009deb85cdd7b7a6149686a062aa4ffe10b
MilosJo <jovancevicmilos@gmail.com>
2018-09-15 18:45:37 +0200

    end of section 7 - redux introduction and testing it out

---
+++ src/app/common/util/reducerUtil.js
@@ -0,0 +1,7 @@
+export const createReducer = (initialState, fnMap) => {
+    return(state = initialState, {type, payLoad}) => {
+        const handler = fnMap[type];
+
+        return handler ? handler(state, payLoad) : state;
+    }
+}
