185fb4c268a19886e90689ca83381bf3432f4885
MilosJo <jovancevicmilos@gmail.com>
2018-09-18 15:22:46 +0200

    end of section 12 - added small(fake) api

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -2,38 +2,40 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Grid } from "semantic-ui-react";
 import EventList from "../EventList/EventList";
-import { deleteEvent } from '../eventActions';
+import { deleteEvent } from "../eventActions";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 
 const mapState = state => ({
-  events: state.events
+  events: state.events,
+  loading: state.async.loading,
+  error: state.async.error
 });
 
 const actions = {
   deleteEvent,
-}
+};
 
 class EventDashboard extends Component {
-
   handleDeleteEvent = eventId => () => {
     this.props.deleteEvent(eventId);
   };
 
   render() {
-    const { events } = this.props;
+    const { events, error, loading } = this.props;
+    if (error) return <span>{error}</span>;
+    if (loading) return <LoadingComponent inverted={true} />;
     return (
       <Grid>
         <Grid.Column width={10}>
-          <EventList
-            deleteEvent={this.handleDeleteEvent}
-            events={events}
-          />
+          <EventList deleteEvent={this.handleDeleteEvent} events={events} />
         </Grid.Column>
-        <Grid.Column width={6}>
-          
-        </Grid.Column>
+        <Grid.Column width={6} />
       </Grid>
     );
   }
 }
 
-export default connect(mapState, actions)(EventDashboard);
+export default connect(
+  mapState,
+  actions
+)(EventDashboard);
