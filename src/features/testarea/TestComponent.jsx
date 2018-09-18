86c42f0890c9124693622281154b33dcb91505cf
MilosJo <jovancevicmilos@gmail.com>
2018-09-18 12:54:26 +0200

    end of section 11 - added modals with redux for login, logout

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,23 +1,22 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import GoogleMapReact from 'google-map-react';
-import { Button, Icon } from "semantic-ui-react";
+import { Button } from "semantic-ui-react";
 import Script from "react-load-script";
 import PlacesAutocomplete, {
   geocodeByAddress,
   getLatLng
 } from "react-places-autocomplete";
 import { incrementCounter, decrementCounter } from "./testActions";
+import { openModal } from '../modals/modalActions';
 
 const mapState = state => ({
   data: state.test.data
 });
 
-const Marker = () => <Icon name='marker' size='big' color='red' />
-
 const actions = {
   incrementCounter,
-  decrementCounter
+  decrementCounter,
+  openModal
 };
 
 class TestComponent extends Component {
@@ -55,7 +54,7 @@ class TestComponent extends Component {
       value: this.state.address,
       onChange: this.onChange
     };
-    const { incrementCounter, decrementCounter, data } = this.props;
+    const { incrementCounter, decrementCounter, data, openModal } = this.props;
     return (
       <div>
         <Script
@@ -66,6 +65,7 @@ class TestComponent extends Component {
         <h3>The answer is: {data}</h3>
         <Button onClick={incrementCounter} color="green" content="Increment" />
         <Button onClick={decrementCounter} color="red" content="Decrement" />
+        <Button onClick={() => openModal('TestModal', {data: 43})} color="teal" content="Open Modal" />
         <br />
         <br />
         <form onSubmit={this.handleFormSubmit}>
@@ -74,20 +74,6 @@ class TestComponent extends Component {
           )}
           <button type="submit">Submit</button>
         </form>
-
-        <div style={{ height: '300px', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: apiKey }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <Marker
-            lat={59.955413}
-            lng={30.337844}
-            text={'Kreyser Avrora'}
-          />
-        </GoogleMapReact>
-      </div>
       </div>
     );
   }
